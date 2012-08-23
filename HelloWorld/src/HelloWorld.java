a47cc8bf353fcca18475b342bdc3e727a0e61ee5
bhilliard <betsy@cs.brown.edu>
2012-08-23 15:36:55 -0400

    Created HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author betsy
+ *Prints a hello message
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args input from console
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
