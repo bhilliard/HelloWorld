267364961c9b74041e85f5f7590e6703328cc271
bhilliard <betsy@cs.brown.edu>
2012-08-23 17:06:01 -0400

    ending before it gets too odd

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
 	 * @param args input from console
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Multi-multi-verse Studios!");
+		System.out.println("Goodbye!");
 
 	}
 
